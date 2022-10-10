1ac92a5eb5b517590ee79ebbca11553ae866f9e7
Brandon Herrera <brandondh769@gmail.com>
2022-10-10 18:16:03 -0500

    Add menu to bottom navigation bar

--- app/src/main/java/com/brandonhxrr/gallery/MainActivity.kt
+++ app/src/main/java/com/brandonhxrr/gallery/MainActivity.kt
@@ -26,7 +26,6 @@ class MainActivity : AppCompatActivity() {
 
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
 
     }
 
