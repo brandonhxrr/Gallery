d8fb71d79852f3622e5e72368e59ae10ecf9f4d6
Brandon Herrera <brandondh769@gmail.com>
2022-10-15 17:48:54 -0500

    Add splash activity

---
+++ app/src/main/java/com/brandonhxrr/gallery/Splash.kt
@@ -0,0 +1,11 @@
+package com.brandonhxrr.gallery
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
