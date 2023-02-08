ce381388b2763b093de9290a0edda0817ef6d870
Brandon Herrera <brandondh769@gmail.com>
2023-02-07 18:12:50 -0600

    Update permissions request

--- app/src/main/java/com/brandonhxrr/gallery/Splash.kt
+++ app/src/main/java/com/brandonhxrr/gallery/Splash.kt
@@ -1,7 +1,6 @@
 package com.brandonhxrr.gallery
 
 import android.Manifest
-import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
@@ -11,8 +10,7 @@ import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import java.io.File
 
 open class Splash : AppCompatActivity() {
-    private val REQUEST_STORAGE_READ_ACCESS_PERMISSION = 101
-    private val REQUEST_STORAGE_WRITE_ACCESS_PERMISSION = 102
+    private val REQUEST_PERMISSIONS = 101
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,36 +23,30 @@ open class Splash : AppCompatActivity() {
     }
 
     private fun checkPermissions() {
-        if (!hasPermission(Manifest.permission.READ_EXTERNAL_STORAGE)) {
-            requestPermission(Manifest.permission.READ_EXTERNAL_STORAGE, getString(R.string.permission_rational_read_external), REQUEST_STORAGE_READ_ACCESS_PERMISSION)
-        } else if (!hasPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
-            requestPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, getString(R.string.permission_rational_read_external), REQUEST_STORAGE_WRITE_ACCESS_PERMISSION)
-        } else {
+        if (!hasPermission(Manifest.permission.READ_EXTERNAL_STORAGE) || !hasPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) || !hasPermission(Manifest.permission.MANAGE_DOCUMENTS)) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.MANAGE_DOCUMENTS), REQUEST_PERMISSIONS)
+        }else {
             loadData()
         }
     }
 
     private fun hasPermission(permission: String): Boolean =
-        ActivityCompat.checkSelfPermission(this@Splash, permission) == PackageManager.PERMISSION_GRANTED
-
-    private fun requestPermission(permission: String, rationale: String, requestCode: Int) {
-        ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
-    }
+        ActivityCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
             when (requestCode) {
-                REQUEST_STORAGE_READ_ACCESS_PERMISSION, REQUEST_STORAGE_WRITE_ACCESS_PERMISSION -> loadData()
+                REQUEST_PERMISSIONS -> loadData()
             }
         } else {
-            showAlertDialog(
-                title = "Permission denied",
-                message = "Permission to access storage is required to continue",
-                onPositiveButtonClickListener = { _, _ -> checkPermissions() },
-                positiveText = "Retry",
-                onNegativeButtonClickListener = { _, _ -> finish() },
-                negativeText = "Exit"
-            )
+            MaterialAlertDialogBuilder(this)
+                .setTitle("Permission denied")
+                .setMessage("Permission to access storage is required to continue")
+                .setPositiveButton("Retry"){ _, _ ->
+                    checkPermissions()
+                }.setNegativeButton("Exit"){ _, _ ->
+                    finish()
+                }.setCancelable(false).show()
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
@@ -69,19 +61,5 @@ open class Splash : AppCompatActivity() {
             }
         startActivity(intent)
         finish()
-    }
-
-    private fun showAlertDialog(title: String?, message: String?,
-                                onPositiveButtonClickListener: DialogInterface.OnClickListener?,
-                                positiveText: String,
-                                onNegativeButtonClickListener: DialogInterface.OnClickListener?,
-                                negativeText: String) {
-        MaterialAlertDialogBuilder(this)
-            .setTitle(title)
-            .setMessage(message)
-            .setPositiveButton(positiveText, onPositiveButtonClickListener)
-            .setNegativeButton(negativeText, onNegativeButtonClickListener)
-            .setCancelable(false)
-            .show()
     }
 }
