0819515c3bf0d4155397bc251dc48faecbcfe198
Brandon Herrera <brandondh769@gmail.com>
2023-08-22 16:51:20 -0600

    Open videos in the media detail screen, instead of opening it directly

--- app/src/main/java/com/brandonhxrr/gallery/adapter/photo/PhotoViewHolder.kt
+++ app/src/main/java/com/brandonhxrr/gallery/adapter/photo/PhotoViewHolder.kt
@@ -5,7 +5,6 @@ import android.app.Activity
 import android.app.ActivityOptions
 import android.content.Intent
 import android.graphics.Bitmap
-import android.net.Uri
 import android.view.View
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
@@ -15,10 +14,14 @@ import com.bumptech.glide.RequestBuilder
 import com.google.gson.Gson
 import java.io.File
 
-class PhotoViewHolder(view: View, val adapter: PhotoAdapter, private val showDeleteMenu: (Boolean, Number) -> Unit) : RecyclerView.ViewHolder(view){
+class PhotoViewHolder(
+    view: View,
+    val adapter: PhotoAdapter,
+    private val showDeleteMenu: (Boolean, Number) -> Unit
+) : RecyclerView.ViewHolder(view) {
 
-    private val image : ImageView = view.findViewById(R.id.item_photo)
-    private val videoPlaceholder : ImageView = view.findViewById(R.id.placeholder)
+    private val image: ImageView = view.findViewById(R.id.item_photo)
+    private val videoPlaceholder: ImageView = view.findViewById(R.id.placeholder)
     private val fileSelected: ImageView = view.findViewById(R.id.selected)
 
     @SuppressLint("NotifyDataSetChanged")
@@ -32,20 +35,20 @@ class PhotoViewHolder(view: View, val adapter: PhotoAdapter, private val showDel
 
         glide.load(photoModel.path).centerCrop().into(image)
 
-        if(selectable){
+        if (selectable) {
             fileSelected.visibility = View.VISIBLE
-        }else {
+        } else {
             fileSelected.visibility = View.GONE
         }
 
-        if(extension in imageExtensions){
+        if (extension in imageExtensions) {
             videoPlaceholder.visibility = View.GONE
-        }else {
+        } else {
             videoPlaceholder.visibility = View.VISIBLE
         }
 
         image.setOnLongClickListener {
-            if(itemsList.isEmpty()){
+            if (itemsList.isEmpty()) {
                 dataList[photoModel.position].selected = true
                 adapter.setSelectedItem(photoModel.position)
                 Glide.with(image.context).load(R.drawable.file_selected).into(fileSelected)
@@ -56,51 +59,45 @@ class PhotoViewHolder(view: View, val adapter: PhotoAdapter, private val showDel
             true
         }
 
-        if(photoModel.selected){
+        if (photoModel.selected) {
             Glide.with(image.context).load(R.drawable.file_selected).into(fileSelected)
-        }else {
+        } else {
             Glide.with(image.context).load(R.drawable.file_unselected).into(fileSelected)
         }
 
         image.setOnClickListener {
-            if(extension in imageExtensions){
-                val limit : Int = if(dataList.size > 1000) 1000 else dataList.size
-                val gson = Gson()
-                val data = gson.toJson(dataList.subList(0, limit))
+            val limit: Int = if (dataList.size > 1000) 1000 else dataList.size
+            val gson = Gson()
+            val data = gson.toJson(dataList.subList(0, limit))
 
-                val activityOptions: ActivityOptions = ActivityOptions.makeSceneTransitionAnimation(it.context as Activity)
+            val activityOptions: ActivityOptions =
+                ActivityOptions.makeSceneTransitionAnimation(it.context as Activity)
 
-                val intent = Intent(it.context, PhotoView::class.java)
-                intent.putExtra("path", photoModel.path)
-                intent.putExtra("data", data)
-                intent.putExtra("position", photoModel.position)
-                it.context.startActivity(intent, activityOptions.toBundle())
-            } else {
-                val videoUri: Uri = Uri.parse(photoModel.path)
-                val intent = Intent(Intent.ACTION_VIEW,videoUri)
-                intent.setDataAndType(videoUri, "video/*")
-                it.context.startActivity(intent)
-            }
+            val intent = Intent(it.context, PhotoView::class.java)
+            intent.putExtra("path", photoModel.path)
+            intent.putExtra("data", data)
+            intent.putExtra("position", photoModel.position)
+            it.context.startActivity(intent, activityOptions.toBundle())
         }
 
         fileSelected.setOnClickListener {
             photoModel.selected = !photoModel.selected
 
-            if(photoModel.selected){
+            if (photoModel.selected) {
                 dataList[photoModel.position].selected = true
                 adapter.setSelectedItem(photoModel.position)
                 Glide.with(image.context).load(R.drawable.file_selected).into(fileSelected)
-            } else{
+            } else {
                 dataList[photoModel.position].selected = false
                 adapter.removeSelectedItem(photoModel.position)
                 Glide.with(image.context).load(R.drawable.file_unselected).into(fileSelected)
             }
 
-            if(itemsList.isEmpty()){
+            if (itemsList.isEmpty()) {
                 selectable = false
-                showDeleteMenu(false, itemsList.size)
+                showDeleteMenu(false, 0)
                 bindingAdapter?.notifyDataSetChanged()
-            }else {
+            } else {
                 showDeleteMenu(true, itemsList.size)
             }
         }
